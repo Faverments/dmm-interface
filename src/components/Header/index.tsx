add1171fa275ea146e1bb3d8444b692c301c06b6
vukhaihoan <funny.khaihoan@gmail.com>
2022-11-30 10:46:14 +0700

    rename brand to kardiachain

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -368,7 +368,7 @@ export default function Header() {
             {/* <IconImage src={isDark ? '/logo-dark.svg' : '/logo.svg'} alt="logo" /> */}
             <Flex style={{ gap: 8 }} alignItems={'center'}>
               <img src={Faverments} alt="logo" height={40} />
-              <StyledTitle>FaverMents</StyledTitle>
+              {/* <StyledTitle>FaverMents</StyledTitle> */}
             </Flex>
           </UniIcon>
         </Title>
